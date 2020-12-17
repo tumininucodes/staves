e716cc35cf089f3670595fd9deca49b6850b408e
Tumi <oluwatumininuojo25@gmail.com>
2020-12-17 21:50:38 +0100

    The rapidity to which we humans forget things is astonishing. The faintest ink is better than the greatest memory. Technically, this isn't hard copy paper and pen but it does the job. An efficient note taking app

--- app/src/main/java/com/tecx/notes/AddNotesActivity.kt
+++ app/src/main/java/com/tecx/notes/AddNotesActivity.kt
@@ -1,10 +1,12 @@
 package com.tecx.notes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.tecx.notes.databinding.ActivityAddNotesBinding
 
+const val REMOVE_IMAGE = "false"
 
 class AddNotesActivity : AppCompatActivity() {
 
@@ -16,5 +18,20 @@ class AddNotesActivity : AppCompatActivity() {
         addNotesBinding = DataBindingUtil.setContentView(this, R.layout.activity_add_notes)
         setContentView(addNotesBinding.root)
 
+
+        addNotesBinding.saveNoteButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+        finishActivity(0)
     }
 }
--- app/src/main/java/com/tecx/notes/MainActivity.kt
+++ app/src/main/java/com/tecx/notes/MainActivity.kt
@@ -2,6 +2,7 @@ package com.tecx.notes
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.tecx.notes.databinding.ActivityMainBinding
@@ -15,14 +16,24 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         mainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         setContentView(mainBinding.root)
+        setSupportActionBar(mainBinding.toolbarNote)
 
+        val isRemoved = intent.getStringExtra(REMOVE_IMAGE).toBoolean()
+
+        if (isRemoved) {
+            mainBinding.addNotesImage.visibility = View.GONE
+        }
 
         mainBinding.fabNotesAdd.setOnClickListener {
             startActivity(Intent(this, AddNotesActivity::class.java))
             finish()
         }
 
+    }
 
+    override fun onBackPressed() {
+        super.onBackPressed()
+        finishActivity(0)
     }
 
 
