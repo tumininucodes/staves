af111a72ef12c6fb4d2ffd77f2883e037fb9c27a
Tumi <oluwatumininuojo25@gmail.com>
2020-12-18 00:26:48 +0100

    The rapidity to which we humans forget things is astonishing. The faintest ink is better than the greatest memory. Technically, this isn't hard copy paper and pen but it does the job. An efficient note taking app

--- app/src/main/java/com/tecx/notes/WelcomeActivity.kt
+++ app/src/main/java/com/tecx/notes/WelcomeActivity.kt
@@ -1,12 +1,25 @@
 package com.tecx.notes
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.tecx.notes.databinding.ActivityWelcomeBinding
 
 class WelcomeActivity : AppCompatActivity() {
 
+    lateinit var welcomeBinding: ActivityWelcomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_welcome)
+        welcomeBinding = DataBindingUtil.setContentView(this, R.layout.activity_welcome)
+        setContentView(welcomeBinding.root)
+
+
+        welcomeBinding.continueButton.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+            overridePendingTransition(R.anim.right_to_left, R.anim.left_to_right)
+        }
     }
 }
