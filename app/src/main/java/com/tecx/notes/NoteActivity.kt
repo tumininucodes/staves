eb6d017a959f3427ec7e0d42a97b8fe25ce02b32
Tumi <oluwatumininuojo25@gmail.com>
2020-12-18 08:00:17 +0100

    The rapidity to which we humans forget things is astonishing. The faintest ink is better than the greatest memory. Technically, this isn't hard copy paper and pen but it does the job. An efficient note taking app

--- app/src/main/java/com/tecx/notes/NoteActivity.kt
+++ app/src/main/java/com/tecx/notes/NoteActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
 import com.tecx.notes.databinding.ActivityNoteBinding
 
@@ -16,9 +17,20 @@ class NoteActivity : AppCompatActivity() {
         noteBinding = DataBindingUtil.setContentView(this, R.layout.activity_note)
         setContentView(noteBinding.root)
 
+        noteBinding.toolbarNote.setNavigationOnClickListener {
+            noteBinding.drawerLayout.openDrawer(GravityCompat.START)
+        }
+
         noteBinding.fabNotesAdd.setOnClickListener {
             startActivity(Intent(this, AddNotesActivity::class.java))
             finish()
+        }
+
+        noteBinding.navigationView.setNavigationItemSelectedListener { menuItem ->
+            // Handle menu item selected
+            menuItem.isChecked = true
+            noteBinding.drawerLayout.closeDrawer(-1)
+            true
         }
     }
 
